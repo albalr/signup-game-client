70e834e78da2dcae0c79bea8e0b46cfbd0bb9141
Alba <albalopez656@gmail.com>
2025-04-25 08:51:52 +0200

    Finishing 7a client (before break) part.

--- src/main/java/dk/dtu/compute/course02324/part4/consuming_rest/GameSignUpClient.java
+++ src/main/java/dk/dtu/compute/course02324/part4/consuming_rest/GameSignUpClient.java
@@ -10,6 +10,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -23,35 +24,17 @@ import java.util.Map;
 
 public class GameSignUpClient extends Application {
 
-    // see
-    // https://docs.spring.io/spring-framework/reference/integration/rest-clients.html#rest-resttemplate
+    private VBox gameListContainer;
 
     public static void main(String[] args) {
-
         launch(args);
-
-        // TODO try to read out the available games from the backend, show them on a
-        //  simple graphical GUI and sign up for a game using some of the operations
-        //  at the top. -- done
-        //  For the GUI to work in JavaFX, you need to add some maven dependencies
-        //  (see pom file for Assignment 3). -- done
-
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        // Set up the REST client for connecting to the backend
         RestClient customClient = RestClient.builder()
                 .baseUrl("http://localhost:8080")
                 .build();
-
-        // Get the list of games from the backend
-        List<Game> games = customClient.get()
-                .uri("/game")
-                .retrieve()
-                .body(HALWrapperGames.class)
-                .getGames();
 
         VBox root = new VBox();
         root.setSpacing(10);
@@ -59,81 +42,35 @@ public class GameSignUpClient extends Application {
 
         Label title = new Label("RoboRally Games");
 
-        // Create Game Button
         Button createGameButton = new Button("Create Game");
-        createGameButton.setOnAction(event -> {
-            // System.out.println("Create Game button clicked");
+        createGameButton.setOnAction(event -> showCreateGameDialog(customClient));
 
-            Stage createGameStage = new Stage();
-            createGameStage.setTitle("Create New Game");
+        gameListContainer = new VBox();
+        gameListContainer.setSpacing(10);
 
-            GridPane createGameLayout = new GridPane();
-            createGameLayout.setHgap(10); // Space between label and text field
-            createGameLayout.setVgap(15); // Space between rows
-            createGameLayout.setPadding(new Insets(10));
-            createGameLayout.setPrefWidth(300);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(gameListContainer);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setPrefHeight(250); // Adjust height as needed
 
-            Label nameLabel = new Label("Game Name:");
-            TextField nameField = new TextField();
+        root.getChildren().addAll(title, createGameButton, scrollPane);
 
-            Label minPlayersLabel = new Label("Min Players:");
-            TextField minPlayersField = new TextField();
+        refreshGameList(customClient);
 
-            Label maxPlayersLabel = new Label("Max Players:");
-            TextField maxPlayersField = new TextField();
+        primaryStage.setTitle("RoboRally Game Sign-Up");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
 
-            Button submitGameButton = new Button("Submit Game");
-            submitGameButton.setOnAction(e -> {
-                String gameName = nameField.getText();
-                // catch errors here fx if gameName is empty or null
-                int minPlayers = Integer.parseInt(minPlayersField.getText());
-                // catch errors here fx if minPlayers is empty, null or not a number
-                int maxPlayers = Integer.parseInt(maxPlayersField.getText());
-                // catch errors here fx if maxPlayers is empty, null or not a number
+    private void refreshGameList(RestClient client) {
+        gameListContainer.getChildren().clear();
 
-                //System.out.println("Game Created: " + gameName + ", Min Players: " + minPlayers + ", Max Players: " + maxPlayers);
+        List<Game> games = client.get()
+                .uri("/game")
+                .retrieve()
+                .body(HALWrapperGames.class)
+                .getGames();
 
-                // TODO: Implement the game creation logic here -- done
-                try {
-                    Game newGame = new Game();
-                    newGame.setName(gameName);
-                    newGame.setMinPlayers(minPlayers);
-                    newGame.setMaxPlayers(maxPlayers);
-
-                    customClient.post()
-                            .uri("/game")
-                            .body(newGame)
-                            .retrieve()
-                            .body(Game.class);
-
-                    //System.out.println("Game Successfully Created");
-
-                } catch (Exception ex) {
-                    //System.out.println("Failed to create game: " + ex.getMessage());
-                    ex.printStackTrace();
-                }
-
-                createGameStage.close();
-            });
-
-            createGameLayout.add(nameLabel, 0, 0);
-            createGameLayout.add(nameField, 1, 0);
-            createGameLayout.add(minPlayersLabel, 0, 1);
-            createGameLayout.add(minPlayersField, 1, 1);
-            createGameLayout.add(maxPlayersLabel, 0, 2);
-            createGameLayout.add(maxPlayersField, 1, 2);
-            createGameLayout.add(submitGameButton, 1, 3);
-            //createGameLayout.getChildren().addAll(nameLabel, nameField, minPlayersLabel, minPlayersField, maxPlayersLabel, maxPlayersField, submitGameButton);
-
-            Scene createGameScene = new Scene(createGameLayout);
-            createGameStage.setScene(createGameScene);
-            createGameStage.sizeToScene();
-            createGameStage.show();
-        });
-
-        root.getChildren().addAll(title, createGameButton);
-
-        // Loop through all games and create UI for each
         for (Game game : games) {
             HBox gameBox = new HBox();
             gameBox.setSpacing(10);
@@ -151,139 +88,9 @@ public class GameSignUpClient extends Application {
             gameInfo.add(new Label("Max Players:"), 0, 2);
             gameInfo.add(new Label(String.valueOf(game.getMaxPlayers())), 1, 2);
 
-            // gameInfo.addRow(0, "Min Players: " + game.getMinPlayers());
-            // gameInfo.addRow(1, "Max Players: " + game.getMaxPlayers());
-            // gameInfo.setPrefWidth(150);
-            // gameInfo.setPrefHeight(50);
-
             Button signUpButton = new Button("Sign Up");
+            signUpButton.setOnAction(event -> showSignUpDialog(client, game));
 
-            signUpButton.setOnAction(event -> {
-                System.out.println("Sign Up button clicked for game: " + game.getName());
-
-                Stage signUpStage = new Stage();
-                signUpStage.setTitle("Sign Up for Game");
-
-                GridPane signUpLayout = new GridPane();
-                signUpLayout.setHgap(10);
-                signUpLayout.setVgap(15);
-                signUpLayout.setPadding(new Insets(10));
-
-                GridPane labelContainer = new GridPane();
-                labelContainer.setHgap(10);
-                labelContainer.setVgap(15);
-                labelContainer.setPadding(new Insets(10));
-                labelContainer.setStyle("-fx-border-color: black; -fx-border-width: 1; -fx-border-style: solid; -fx-background-color: white;");
-
-                Label gameNameLabel = new Label("Game: " + game.getName());
-                Label minPlayersLabel = new Label("Min players: " + game.getMinPlayers());
-                Label maxPlayersLabel = new Label("Max players: " + game.getMaxPlayers());
-                // Not implemented yet
-                // Label currentPlayers = new Label("Current players: " + game.getCurrentPlayers());
-
-                labelContainer.add(gameNameLabel, 0, 0);
-                labelContainer.add(minPlayersLabel, 0, 1);
-                labelContainer.add(maxPlayersLabel, 0, 2);
-                // labelContainer.add(currentPlayers, 0, 3);
-
-                Label userNameLabel = new Label("User:");
-                TextField userNameField = new TextField();
-                userNameField.setPromptText("Enter your user");
-                userNameField.setPrefWidth(100);
-
-                Label playerNameLabel = new Label("Player:");
-                TextField playerNameField = new TextField();
-                playerNameField.setPromptText("Enter your player");
-                playerNameField.setPrefWidth(100);
-
-                Button submitSignupButton = new Button("Sign up!");
-                submitSignupButton.setOnAction(e -> {
-                    String userName = userNameField.getText();
-                    // catch errors here fx if userName is empty or null
-                    String playerName = playerNameField.getText();
-                    // catch errors here fx if playerName is empty, null or not a number
-
-                    //System.out.println("Signing up for game: " + game.getName() + ", User: " + userName + ", Player: " + playerName);
-
-                    // TODO: Implement the sign-up logic here -- done
-                    try {
-                        // check if user exists
-                        List<User> users = customClient.get()
-                                .uri("/user")
-                                .retrieve()
-                                .body(HALWrapperUsers.class)
-                                .getUsers();
-
-                        User user = users.stream()
-                                .filter(u -> userName.equalsIgnoreCase(u.getName()))
-                                .findFirst()
-                                .orElse(null);
-
-                        // if the user does not exist, create a new one
-                        if (user == null) {
-                            User newUser = new User();
-                            newUser.setName(userName);
-
-                            customClient.post()
-                                    .uri("/user")
-                                    .body(newUser)
-                                    .retrieve()
-                                    .toBodilessEntity(); // don't expect a return body
-
-                            // fetch again to obtain UID
-                            user = customClient.get()
-                                    .uri("/user")
-                                    .retrieve()
-                                    .body(HALWrapperUsers.class)
-                                    .getUsers()
-                                    .stream()
-                                    .filter(u -> userName.equalsIgnoreCase(u.getName()))
-                                    .findFirst()
-                                    .orElseThrow(() -> new RuntimeException("Failed to create and retrieve user"));
-                        }
-
-                        // create a new Player linked to the user and game
-                        Map<String, Object> playerData = new HashMap<>();
-                        playerData.put("name", playerName);
-                        playerData.put("user", "/user/" + user.getUid());
-                        playerData.put("game", "/game/" + game.getUid());
-
-                        Player createdPlayer = customClient.post()
-                                .uri("/player")
-                                .body(playerData)
-                                .retrieve()
-                                .body(Player.class);
-
-                        //System.out.println("Player signed up: " + createdPlayer.getName());
-
-                    } catch (Exception ex) {
-                        System.err.println("Error during sign-up: " + ex.getMessage());
-                        ex.printStackTrace();
-                    }
-
-                    signUpStage.close();
-                });
-
-                signUpLayout.add(labelContainer, 0, 0, 2, 1); // Spanning 2 columns for the label container
-                signUpLayout.add(userNameLabel, 0, 1);
-                signUpLayout.add(userNameField, 1, 1);
-                signUpLayout.add(playerNameLabel, 0, 2);
-                signUpLayout.add(playerNameField, 1, 2);
-                signUpLayout.add(submitSignupButton, 1, 3);
-
-                Scene signUpScene = new Scene(signUpLayout);
-                signUpStage.setScene(signUpScene);
-                signUpStage.sizeToScene();
-                signUpStage.show();
-
-            });
-
-            //gameInfo.add(signUpButton, 0, 3, 2, 1);
-
-//            ScrollPane gameScrollPane = new ScrollPane();
-//            gameScrollPane.setContent(gameInfo);
-//            gameScrollPane.setPrefWidth(200);
-//            gameScrollPane.setFitToHeight(true);
             VBox buttonBox = new VBox();
             buttonBox.setPadding(new Insets(30, 0, 0, 0));
             buttonBox.getChildren().add(signUpButton);
@@ -295,14 +102,145 @@ public class GameSignUpClient extends Application {
             gameBox.getChildren().add(gamePane);
             gameBox.setStyle("-fx-border-color: black; -fx-border-width: 1; -fx-border-style: solid; -fx-background-color: white;");
 
-            //gameBox.getChildren().addAll(gameScrollPane);
-            root.getChildren().add(gameBox);
+            gameListContainer.getChildren().add(gameBox);
         }
+    }
 
-        // vbox1.getChildren().addAll(title, gameList, addGameBtn, signUpBtn);
+    private void showCreateGameDialog(RestClient client) {
+        Stage createGameStage = new Stage();
+        createGameStage.setTitle("Create New Game");
 
-        primaryStage.setTitle("RoboRally Game Sign-Up");
-        primaryStage.setScene(new Scene(root, 400, 300));
-        primaryStage.show();
+        GridPane layout = new GridPane();
+        layout.setHgap(10);
+        layout.setVgap(15);
+        layout.setPadding(new Insets(10));
+        layout.setPrefWidth(300);
+
+        TextField nameField = new TextField();
+        TextField minPlayersField = new TextField();
+        TextField maxPlayersField = new TextField();
+
+        Button submitButton = new Button("Submit Game");
+        submitButton.setOnAction(e -> {
+            try {
+                Game newGame = new Game();
+                newGame.setName(nameField.getText());
+                newGame.setMinPlayers(Integer.parseInt(minPlayersField.getText()));
+                newGame.setMaxPlayers(Integer.parseInt(maxPlayersField.getText()));
+
+                client.post()
+                        .uri("/game")
+                        .body(newGame)
+                        .retrieve()
+                        .body(Game.class);
+
+                refreshGameList(client);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            createGameStage.close();
+        });
+
+        layout.add(new Label("Game Name:"), 0, 0);
+        layout.add(nameField, 1, 0);
+        layout.add(new Label("Min Players:"), 0, 1);
+        layout.add(minPlayersField, 1, 1);
+        layout.add(new Label("Max Players:"), 0, 2);
+        layout.add(maxPlayersField, 1, 2);
+        layout.add(submitButton, 1, 3);
+
+        createGameStage.setScene(new Scene(layout));
+        createGameStage.sizeToScene();
+        createGameStage.show();
+    }
+
+    private void showSignUpDialog(RestClient client, Game game) {
+        Stage signUpStage = new Stage();
+        signUpStage.setTitle("Sign Up for Game");
+
+        GridPane layout = new GridPane();
+        layout.setHgap(10);
+        layout.setVgap(15);
+        layout.setPadding(new Insets(10));
+
+        GridPane labelBox = new GridPane();
+        labelBox.setHgap(10);
+        labelBox.setVgap(15);
+        labelBox.setPadding(new Insets(10));
+        labelBox.setStyle("-fx-border-color: black; -fx-border-width: 1; -fx-border-style: solid; -fx-background-color: white;");
+
+        labelBox.add(new Label("Game: " + game.getName()), 0, 0);
+        labelBox.add(new Label("Min players: " + game.getMinPlayers()), 0, 1);
+        labelBox.add(new Label("Max players: " + game.getMaxPlayers()), 0, 2);
+
+        TextField userField = new TextField();
+        userField.setPromptText("Enter your user");
+
+        TextField playerField = new TextField();
+        playerField.setPromptText("Enter your player");
+
+        Button submitButton = new Button("Sign up!");
+        submitButton.setOnAction(e -> {
+            try {
+                String userName = userField.getText();
+                String playerName = playerField.getText();
+
+                List<User> users = client.get()
+                        .uri("/user")
+                        .retrieve()
+                        .body(HALWrapperUsers.class)
+                        .getUsers();
+
+                User user = users.stream()
+                        .filter(u -> userName.equalsIgnoreCase(u.getName()))
+                        .findFirst()
+                        .orElse(null);
+
+                if (user == null) {
+                    User newUser = new User();
+                    newUser.setName(userName);
+                    client.post().uri("/user").body(newUser).retrieve().toBodilessEntity();
+
+                    user = client.get()
+                            .uri("/user")
+                            .retrieve()
+                            .body(HALWrapperUsers.class)
+                            .getUsers()
+                            .stream()
+                            .filter(u -> userName.equalsIgnoreCase(u.getName()))
+                            .findFirst()
+                            .orElseThrow(() -> new RuntimeException("Failed to create and retrieve user"));
+                }
+
+                Map<String, Object> playerData = new HashMap<>();
+                playerData.put("name", playerName);
+                playerData.put("user", "/user/" + user.getUid());
+                playerData.put("game", "/game/" + game.getUid());
+
+                client.post()
+                        .uri("/player")
+                        .body(playerData)
+                        .retrieve()
+                        .body(Player.class);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            signUpStage.close();
+        });
+
+        layout.add(labelBox, 0, 0, 2, 1);
+        layout.add(new Label("User:"), 0, 1);
+        layout.add(userField, 1, 1);
+        layout.add(new Label("Player:"), 0, 2);
+        layout.add(playerField, 1, 2);
+        layout.add(submitButton, 1, 3);
+
+        signUpStage.setScene(new Scene(layout));
+        signUpStage.sizeToScene();
+        signUpStage.show();
     }
 }
